77ff33712cc0ad4d4432e4eeaadc7a0d11b9ecfd
chan24 <aakaschan24@gmail.com>
2017-03-27 13:14:59 +0530

    maps

--- app/src/main/java/com/example/chan24/smartplanner/LoginActivity.java
+++ app/src/main/java/com/example/chan24/smartplanner/LoginActivity.java
@@ -1,8 +1,11 @@
 
 package com.example.chan24.smartplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
