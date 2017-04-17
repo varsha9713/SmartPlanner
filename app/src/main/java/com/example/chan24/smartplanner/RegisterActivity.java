226565b4918eafbc550c499150ed75db694c6625
Niharika K <Niharika K>
2017-04-17 22:30:31 +0530

    Database

--- app/src/main/java/com/example/chan24/smartplanner/RegisterActivity.java
+++ app/src/main/java/com/example/chan24/smartplanner/RegisterActivity.java
@@ -2,19 +2,32 @@ package com.example.chan24.smartplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    DatabaseHelper db= new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        final EditText user = (EditText)findViewById(R.id.user);
-        final EditText pass = (EditText)findViewById(R.id.pass);
-        final EditText mail = (EditText)findViewById(R.id.mail);
-        final Button signup =(Button)findViewById(R.id.signup);
+        //final EditText user = (EditText)findViewById(R.id.user);
+        //final EditText pass = (EditText)findViewById(R.id.pass);
+        //final EditText mail = (EditText)findViewById(R.id.mail);
+        //final Button signup =(Button)findViewById(R.id.signup);
 
+    }
+    public void signup(View view)
+    {
+        EditText user = (EditText)findViewById(R.id.user);
+        EditText pass = (EditText)findViewById(R.id.pass);
+        EditText mail = (EditText)findViewById(R.id.mail);
+
+        boolean res =db.insertdata(user.getText().toString(),pass.getText().toString(),mail.getText().toString());
+        if(res)
+            Toast.makeText(this, "Inserted ", Toast.LENGTH_SHORT).show();
     }
 }
