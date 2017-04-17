5199faa5e4b977826834f64a3eeb5fb519c569ab
Niharika K <Niharika K>
2017-04-17 21:03:42 +0530

    test

--- app/src/main/java/com/example/chan24/smartplanner/ProfileActivity.java
+++ app/src/main/java/com/example/chan24/smartplanner/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.chan24.smartplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Toast.makeText(getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
     }
 }
